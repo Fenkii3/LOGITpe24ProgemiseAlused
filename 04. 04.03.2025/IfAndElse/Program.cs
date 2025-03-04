46aa4c13ab67b8cbc262da4602447bec5cb4b4d1
Opilane TTHK <opilane@tthk.ee>
2025-03-04 10:34:39 +0200

    IfAndElse

---
+++ 04. 04.03.2025/IfAndElse/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace IfAndElse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //if ja else rakendus tuleb teha
+            //kontrollib sinu vanust
+            //1. kui old alla 18, siis konsool annab vastuseks,
+            //et oled alaealine
+
+            //2. Kui oled 19 kuni 65 aastat vana, siis
+            //konsool vastab, et oled täisealine
+
+            //3. Kui oled üle 65a vana, siis pensionäär
+            
+            Console.WriteLine("Sisestage oma vanus");
+            int age = int.Parse(Console.ReadLine());
+
+            if (age < 18)
+            Console.WriteLine("Olete alaealine");
+
+
+            else if (age >= 18 & age <= 65)
+            Console.WriteLine("Olete täisealine");
+
+            else if (age >= 66 - 100)
+            Console.WriteLine("Olete pensionäär");
+            
+
+        }
+    }
+}
