70a772c7c0e90bdc4d4f021bef605aa35f959eb3
Opilane TTHK <opilane@tthk.ee>
2025-03-04 11:24:18 +0200

    MethodCall

---
+++ 04. 04.03.2025/MethodCall/MethodCall/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace MethodCall
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //kutsuda esile meetod jalgratas
+
+            Jalgratas();
+        }
+
+        static void Jalgratas()
+        {
+            Console.WriteLine("Nüüd olen jalgratta meetodis");
+        }
+    }
+}
