aa40b4b8bda92f5132ce821ec9438017fd21c5e3
Opilane TTHK <opilane@tthk.ee>
2025-03-04 11:09:41 +0200

    IfElseOddNumber

---
+++ 04. 04.03.2025/IfElseOddNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿
+            //konsool küsib numrbit ja siin muudetakse
+            //see int andmetüübiks
+
+            //teha if ja else-ga kontroll,
+            //kus saame teada, et kas nr
+            //on paaris või paaritu
+            //kasuta operaatoreid
+
+            
+            Console.Write("Sisesta number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("Number on paaris");
+            }
+            else
+            {
+                Console.WriteLine("Number on paaritu");  }
+
+
+
+
+
+
+
+
+
+
+
+
+
