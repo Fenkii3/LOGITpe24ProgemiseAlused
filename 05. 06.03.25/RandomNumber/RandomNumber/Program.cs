331624df614e0279513524ba03af98129a1036d1
Opilane TTHK <opilane@tthk.ee>
2025-03-06 09:05:58 +0200

    RandomNumber

---
+++ 05. 06.03.25/RandomNumber/RandomNumber/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace RandomNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int number = new Random().Next(1, 7);
+
+            //nüüd tuleb kasutada switchi, et näidata numbrit 1-st kuni 6-ni.
+
+            switch (number)
+            {
+                case 1:
+                    Console.WriteLine("Veeretasid nr 1");
+                    break;
+                case 2:
+                    Console.WriteLine("Veeretasid nr 2");
+                    break;
+                case 3:
+                    Console.WriteLine("Veeretasid nr 3");
+                    break;
+                case 4:
+                    Console.WriteLine("Veeretasid nr 4");
+                    break;
+                case 5:
+                    Console.WriteLine("Veeretasid nr 5");
+                    break;
+                case 6:
+                    Console.WriteLine("Veeretasid nr 6");
+                    break;
+                default:
+                    Console.WriteLine("ERROOOR");
+                    break;
+                }
+            }
+        }
+}
+
+
+
+
